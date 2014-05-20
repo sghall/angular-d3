be0ec4a4b188bedc6b0787ac81bfbd97c1db5c30
shall <hall.steven.g@gmail.com>
2014-05-20 11:51:09 -0700

    force chart working

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngAnimate', 'fx.animations']);
+var myApp = angular.module('myApp', ['ngAnimate', 'fx.animations', 'ngSanitize']);
 
 myApp.controller('MainCtrl', function($scope, $http){
 
@@ -12,7 +12,6 @@ myApp.controller('MainCtrl', function($scope, $http){
         return mnth + "-" + days + "-" + year;
       };
 
-
       console.log(data[0][0].data);
       $scope.episodes = data.sort(function (a, b) {
         return a[0].data.sequence - b[0].data.sequence;
@@ -20,6 +19,22 @@ myApp.controller('MainCtrl', function($scope, $http){
     }).error(function(err){
       throw err;
     });
+  };
+
+  $scope.updateEpisode = function () {
+    console.log("episode", this.ep[0]);
+    $scope.plot = this.ep[0].data.plot;
+    $scope.title = this.ep[0].data.title;
+    $scope.snumber = this.ep[0].data.season;
+    $scope.enumber = this.ep[0].data.sequence;
+      var ul = this.ep[0].self;
+      var id = +ul.substr(ul.lastIndexOf('/') + 1);
+      $http.get('characters/' + id).success(function(data){
+        $scope.episodeCast = data;
+        $scope.episodeID = id;
+      }).error(function(err){
+        throw err;
+      });
   };
 
   $scope.season = 1;
@@ -32,8 +47,6 @@ myApp.controller('MainCtrl', function($scope, $http){
       return mnth + "-" + days + "-" + year;
     };
 
-
-    console.log(data[0][0].data);
     $scope.episodes = data.sort(function (a, b) {
       return a[0].data.sequence - b[0].data.sequence;
     });
@@ -41,12 +54,160 @@ myApp.controller('MainCtrl', function($scope, $http){
     throw err;
   });
 
-  // $http.get('donut-data-2.json').then(function(response){
-  //   $scope.characters = response.data;
-  // }, function(err){
-  //   throw err;
-  // });
 });
+
+myApp.directive('forceChart', function(){
+  function link(scope, el, attr){
+
+    var width = 700, height = 400;
+
+    var svg = d3.select(el[0]).append("svg")
+    svg.attr({id: 'graph-svg', width: width, height: height});
+
+    var link = svg.selectAll(".link");
+    var node = svg.selectAll(".node");
+
+    var force = d3.layout.force()
+        .size([width, height])
+
+    var createNodesLinks = function (data) {
+      var nodes = {}, links = [];
+
+      _.each(data, function (relation) {
+        _.each(relation.graph.nodes, function (node) {
+          if (!nodes[node.id]) nodes[node.id] = node;
+        });
+        addLinks(relation)
+      });
+
+      function addLinks(d) {
+        _.each(d.graph.relationships, function (r) {
+          links.push({source: nodes[r.startNode], target: nodes[r.endNode]})
+        })
+      }
+      return {nodes: _.toArray(nodes), links: links};
+    };
+
+    var dragstart = function (d) {
+      if (d.properties.label === "Recipe"){
+        d3.select(this).classed("fixed", d.fixed = true);
+      }
+    }
+
+    var tick = function() {
+      link.attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+      node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+    };
+
+    var updateNodes = function (curid, force) {
+      node = node.data(force.nodes(), function(d) { return d.id;});
+      node.enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+
+      node.append("circle")
+        .attr("class", function (d) { return d.properties.label === "Episode" ? "episode": "character"; })
+        .attr("r", function (d) { return d.properties.label === "Episode" ? 15: 10; })
+
+      node.append("text")
+        .attr("dx", function (d) { return d.properties.label === "Episode" ? 15: 12; })
+        .attr("dy", ".35em")
+        .text(function (d) { return d.properties.name });
+
+      node.classed("selected", function (d) { return d.id == curid })
+      node.exit().remove();
+
+      link = link.data(force.links(), function (d) { return d.source.id + "-" + d.target.id; });
+      link.enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", "2px");
+      link.exit().remove();
+
+      force.start();
+    };
+
+    scope.$watch('episodeCast', function(data){
+      if (data) {
+        force
+          .charge(-500)
+          .linkDistance(150)
+          .nodes([])
+          .links([])
+          .on("tick", tick)
+          .drag().on("dragstart", dragstart)
+
+        var graph = createNodesLinks(data[0].data)
+        force.nodes(graph.nodes);
+        force.links(graph.links);
+        updateNodes(null, force);
+      }
+    }, true);
+  }
+  return {
+    link: link,
+    restrict: 'E',
+    scope: false
+  };
+});
+
+
+myApp.directive('pieChart', function(){
+  function link(scope, el, attr){
+    var color = d3.scale.category10();
+    var width = 200;
+    var height = 200;
+    var min = Math.min(width, height);
+    var svg = d3.select(el[0]).append('svg');
+    var pie = d3.layout.pie().sort(null);
+    var arc = d3.svg.arc()
+      .outerRadius(min / 2 * 0.9)
+      .innerRadius(min / 2 * 0.5);
+
+    svg.attr({width: width, height: height});
+    var g = svg.append('g')
+      // center the donut chart
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+    
+    // add the <path>s for each arc slice
+    var arcs = g.selectAll('path');
+
+    scope.$watch('seasons', function(data){
+      arcs = arcs.data(pie(data));
+      arcs.enter().append('path')
+        .style('stroke', 'white')
+        .attr('fill', function(d, i){ return color(i) });
+      arcs.exit().remove();
+      arcs.attr('d', arc);
+    }, true);
+  }
+  return {
+    link: link,
+    restrict: 'E',
+    scope: false
+  };
+});
+
+// $(document).ready(function() {
+//   resize();
+// });
+
+// $(window).on("resize", function() {
+//   resize();
+// });
+
+// var resize = function () {
+//   var chart = $("#graph-svg");
+//   chart.attr("width", chart.parent().width());
+//   chart.attr("height", chart.parent().height());
+//   this.force.size([chart.parent().width(), chart.parent().height()]);
+//   this.force.start(); 
+// }
+
+
 
 // myApp.directive('episodeList', function(){
 //   function link(scope, el, attr){
